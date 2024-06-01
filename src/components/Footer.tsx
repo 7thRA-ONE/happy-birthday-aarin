92c07dad5af8a10c762b69be663df09781a8ce3c
RA_ONE <125670877+7thRA-ONE@users.noreply.github.com>
2024-06-01 04:52:13 +0000

    gdgfdfdg

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
         <header className="w-full flex flex-col absolute bg-transparent z-40 opacity-97 bg-gradient-to-b from-[#FF4] to-[#FF9] p-2">
             <nav className="flex items-center justify-center p-4 w-full">
                 <Link href="/" className="flex items-center space-x-1">
-                    <h1 className="text-xl font-semibold">Made with ❤️ by Your Friend</h1>
+                    <h1 className="text-xl font-semibold">Made with ❤️ by Ronin</h1>
                 </Link>
             </nav>
         </header>
