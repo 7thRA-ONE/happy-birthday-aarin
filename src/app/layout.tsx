0e1bc6894b2d98fc3864bb20924b901ab983df1e
RA_ONE <125670877+7thRA-ONE@users.noreply.github.com>
2024-08-10 05:27:15 +0000

    #2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     return (
         <html lang="en">
             <head>
-                <title>UraraDay!!! 🎂</title>
+                <title>RuiDay!!! 🎂</title>
                 <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
                 <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
                 <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
