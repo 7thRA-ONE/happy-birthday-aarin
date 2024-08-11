82367ac99ca70c3423d3fb78221f14b51fe5ab91
RA_ONE <125670877+7thRA-ONE@users.noreply.github.com>
2024-08-11 10:23:10 +0000

    F

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,13 @@
 import Link from 'next/link'
-import type { PropsWithChildren } from 'react'
 
 const Footer = () => {
     return (
         <header className="w-full flex flex-col absolute bg-transparent z-40 opacity-97 bg-gradient-to-b from-[#FF4] to-[#FF9] p-2">
             <nav className="flex items-center justify-center p-4 w-full">
-                <Link href="/" className="flex items-center space-x-1">
-                    <h1 className="text-xl font-semibold">Made with ❤️ by Ronin</h1>
+                <Link href="https://wa.me/13234541422" className="flex items-center space-x-1">
+                    <h1 className="text-xl font-semibold bg-[#854CE6] text-[#F2F5F7] py-2 px-4 rounded-md hover:bg-[#6a3eb5] transition-colors duration-300">
+                        Made with ❤️ by Ronin
+                    </h1>
                 </Link>
             </nav>
         </header>
